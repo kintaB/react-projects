5e147a62e1ffe66404b644c5f227c94e4e3fc8e0
kintaB <batnik63@gmail.com>
2021-07-02 22:27:26 +0300

    fourth commit

--- src/redux/state.js
+++ src/redux/state.js
@@ -10,7 +10,7 @@ let store = {
         { message: "Pigga", id: "3", likeCount: "13" },
         { message: "Rigga", id: "4", likeCount: "14" },
       ],
-      newPostText: "Yagon Don",
+      newPostText: "Yasper Moglot",
     },
     DialogsPage: {
       friends: [
