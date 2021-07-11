011b325c25b78070d021437a2aba64215148fe9e
Nikita Batrak <batnik63@gmai.com>
2021-07-11 18:48:20 +0300

    add render to profile container component

--- src/components/Content/second-rate-components/ProfileContainet.jsx
+++ src/components/Content/second-rate-components/ProfileContainet.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
-class ProfileConteiner extends React.Component {}
+class ProfileConteiner extends React.Component {
+  render() {
+    return <>vse okey</>;
+  }
+}
 
 export default ProfileConteiner;
