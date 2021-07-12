90e445fd655dc939cdbf255c504a279efbb756d5
Nikita Batrak <batnik63@gmai.com>
2021-07-12 20:16:29 +0300

    add authtorization

--- src/components/Content/ContentContainer.jsx
+++ src/components/Content/ContentContainer.jsx
@@ -3,11 +3,16 @@ import Content from "./Content";
 import axios from "axios";
 import { connect } from "react-redux";
 import { setUserProfile } from "../../redux/profileReducer";
+import { withRouter } from "react-router-dom";
 
 class ContentContainer extends React.Component {
   componentDidMount() {
+    let userId = this.props.match.params.userId;
+    if (!userId) {
+      userId = 2;
+    }
     axios
-      .get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+      .get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`)
       .then((response) => {
         this.props.setUserProfile(response.data);
       });
@@ -25,4 +30,8 @@ let mapStateToProps = (state) => ({
   profile: state.profilePage.profile,
 });
 
-export default connect(mapStateToProps, { setUserProfile })(ContentContainer);
+let withUrlDataConteinerComponent = withRouter(ContentContainer);
+
+export default connect(mapStateToProps, { setUserProfile })(
+  withUrlDataConteinerComponent
+);
