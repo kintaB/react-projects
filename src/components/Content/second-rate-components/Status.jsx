f554070d3034f93c4e8ff650473c6a239a2aede5
Nikita Batrak <batnik63@gmai.com>
2021-07-19 07:35:47 +0300

    add status on main profile info, add activated/deactivated focus

---
+++ src/components/Content/second-rate-components/Status.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import s from "../Content.module.css";
+
+class Status extends React.Component {
+  state = {
+    editMode: false,
+  };
+  activateEditMode() {
+    this.setState({
+      editMode: true,
+    });
+  }
+  deactivateEditMode() {
+    this.setState({
+      editMode: false,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        {!this.state.editMode && (
+          <div className={s.status}>
+            <span onDoubleClick={this.activateEditMode.bind(this)}>
+              {this.props.status}
+            </span>
+          </div>
+        )}
+        {this.state.editMode && (
+          <div className={s.status}>
+            <input
+              onBlur={this.deactivateEditMode.bind(this)}
+              value={this.props.status}
+            />
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Status;
